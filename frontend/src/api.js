3c14b1d2d045596fc602186b8a32e844a3a76b3b
Nukala Vishal <nukalavishal6@gmail.com>
2025-12-02 23:14:05 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://xeno-2vhd.onrender.com"
 });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
